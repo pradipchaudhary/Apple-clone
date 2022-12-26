dda4bbdc14902016c8d46a00c75ba75682f10823
Pradip Chaudhary <chaudharypradip678@gmail.com>
2022-12-26 16:27:32 +0545

    ddependencies added.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-
+import Header from './components/Header';
+import CoinPage from './Pages/CoinPage';
+import Homepage from './Pages/Homepage'
 function App() {
   return (
-    <div className="App">
-      Welcome
+    <div className='app'>
+      <Header />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/coins/:id" element={<CoinPage />} />
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
