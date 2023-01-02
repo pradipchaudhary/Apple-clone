dde28e7f405c6705012a6b79f5b03288d5c5cb7f
Pradip Chaudhary <chaudharypradip678@gmail.com>
2023-01-03 00:24:35 +0545

    mui5 theme configure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import { makeStyles } from '@mui/material';
+// import { makeStyles } from '@mui/styles';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import CoinPage from './Pages/CoinPage';
-import Homepage from './Pages/Homepage'
+import Homepage from './Pages/Homepage';
+
+const useStyles = makeStyles({
+  app: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: "100vh",
+    padding: '0 30px',
+  },
+});
+
+
+  
 function App() {
+  const classes = useStyles();
   return (
-    <div className='app'>
-      <Header />
       <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/coins/:id" element={<CoinPage />} />
-      </Routes>
+        <div className={classes.app}>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Homepage/>} exact />
+            <Route path="/coins/:id" component={<CoinPage/>} exact />
+          </Routes>
+        </div>
       </BrowserRouter>
-    </div>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
     <div>
-      <h1>Header</h1>
+      <h1> Header  </h1>
     </div>
   )
 }
